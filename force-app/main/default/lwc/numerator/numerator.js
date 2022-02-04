250e7d02f91b6c191c5454d2cb87c3ead2696762
diegodelias <diegodelias@gmail.com>
2022-02-03 22:09:10 -0300

    segundo commit

--- force-app/main/default/lwc/numerator/numerator.js
+++ force-app/main/default/lwc/numerator/numerator.js
@@ -1,9 +1,19 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
 export default class Numerator extends LightningElement {
 
 
-    counter = 0;
+    //@api counter = 0;
+    _currentCount = 0;
+    priorCount = 0;
+    @api
+    get counter() {
+      return this._currentCount;
+    }
+    set counter(value) {
+      this.priorCount = this._currentCount;
+      this._currentCount = value;
+    }
     handleIncrement() {
       this.counter++;
     }
@@ -16,6 +26,10 @@ export default class Numerator extends LightningElement {
         this.counter *= factor;
       }
     
+      @api
+      maximizeCounter() {
+        this.counter += 1000000;
+      }
 
 
 }
